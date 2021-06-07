83b9303424ff15d8e00657075a8e87aa48c15eec
Shan Siddiqui <shansiddiqui@Shans-Air.lan>
2021-06-07 16:24:13 -0400

    first commit, added tailwindcss

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <button class="text-yellow hover:text-red-500 ...">Hover me</button>
+    </div>
+  );
+}
+
+export default App;
